a09123e89b5e4590887db02f291ae921604b7301
Muskan Goyal <muskangoyal170303@gmail.com>
2024-07-06 12:15:08 +0530

    muskan goyal:commit 1.1

--- src/Pages/AddTournament.jsx
+++ src/Pages/AddTournament.jsx
@@ -1,31 +1,40 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import "./AddTournament.css"
 
 function AddTournament() {
   return (
     <div className='login-page'>
       <a href="/" className='back-to-home'>&lt;&nbsp; Back to GamePlay</a>
       <div className='login-container'>
-        <h3>Welcome to GamePlay</h3>
-        <p>Register</p>
+        <h3>Welcome Organizer</h3>
+        <p>Add a Event</p>
         <div className='input-field'>
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" placeholder='Enter Your Username' />
+          <label htmlFor="name">Event Name</label>
+          <input type="text" id="name" placeholder='Enter Event Name' />
         </div>
         <div className='input-field'>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder='Enter Your Email' />
+          <label htmlFor="time">Time of Event</label>
+          <input type="datetime-local" id="time" />
+        </div>
+        <div className='money'>
+          <div className='input-field'>
+            <label htmlFor="fee">Entry Fee</label>
+            <input type="number" min={0} id="fee" placeholder='Enter Entry Fees' />
+          </div>
+          <div className='input-field'>
+            <label htmlFor="prize">Prize Money</label>
+            <input type="text" id="prize" placeholder='Enter Prize Money' />
+          </div>
         </div>
         <div className='input-field'>
-          <label htmlFor="password">Password</label>
-          <input type="text" id="password" placeholder='Enter your Password' />
+          <label htmlFor="max-team">Max Teams Allowed</label>
+          <input type="number" min={2} id="max-team" placeholder='Enter No. of Teams' />
         </div>
-        <button className='btn'>Signup for Free</button>
         
-        <div className='or-separator'> OR </div>
+        <button className='btn'>Add Event</button>
         
-        <div className='link-div'>Already have an account? <Link className='link' to="/login">Sign In Here</Link></div>
-      </div>
+        </div>
     </div>
   )
 }
